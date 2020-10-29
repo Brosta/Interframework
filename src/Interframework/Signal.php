58e90a33cbd4facdd80a26341385550122be2e9a
John Stamoutsos <john.stamoutsos@gmail.com>
2020-10-29 19:24:37 +0200

    Update Signal.php

--- src/Interframework/Signal.php
+++ src/Interframework/Signal.php
@@ -1,8 +1,11 @@
+<?php
+
+// The code has been deleted for the moment. Comming very soon!
+
 require(__DIR__.'/provider/brosta/complex.php');
 
 Brosta(function($app) {
     _include('index');
 });
 
-
-The code has been deleted for the moment. Comming very soon!
+?>
